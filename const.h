00f2bf019179127fb776896b3f7f227f499b6d0e
Razorro <623571826@qq.com>
2016-06-21 19:32:41 +0800

    Update const.h

--- const.h
+++ const.h
@@ -1,30 +1,30 @@
 #ifndef __CONST_H__
 #define __CONST_H__
 
-/* all const variables of client and server are here */
+
 
 namespace CONST
 {
-/* port number */
+
 	const int PORT_NO = 9999;
 
-/* user name and password size */
+
 	const int USER_NAME_SIZE = 30;
 	const int USER_PSW_SIZE  = 10;
 
-/* the char used to glue */
+
 	const char CHAR_GLUE_MSG      = 0;
 	const char CHAR_GLUE_NAME_PSW = 1;
 	
 /* client state */
-	const int NON_LOGIN_REGIST = 0; /* the initial state of client */
-	const int LOGIN_WAIT_USERNAME = 1; /* client is at login state, waiting for user name */
-	const int LOGIN_WAIT_USERPSW  = 2; /* client is at login state, waiting for user password */
-	const int REGIST_WAIT_USERNAME = 3; /* client is at regist state, waiting for user name */
-	const int REGIST_WAIT_USERPSW  = 4; /* client is at regist state, waiting for user password */
-	const int WAIT_SERVER_RET = 5; /* waiting for the return from server on login or regist */
-	const int COMMUNICATE = 6; /* client is communicating with server */
-	const int QUIT = -1; /* client quit */
+	const int NON_LOGIN_REGIST = 0; 
+	const int LOGIN_WAIT_USERNAME = 1; 
+	const int LOGIN_WAIT_USERPSW  = 2; 
+	const int REGIST_WAIT_USERNAME = 3; 
+	const int REGIST_WAIT_USERPSW  = 4; 
+	const int WAIT_SERVER_RET = 5; 
+	const int COMMUNICATE = 6;
+	const int QUIT = -1; 
 
 /* message */
 	/* message content size */
@@ -59,38 +59,38 @@ namespace CONST
 
 /* user request type */
 	const  int  USER_REQUEST_TYPE_SIZE = 10;
-	extern char USER_REQUEST_TYPE_MSG[]; /* request sending message */
-	extern char USER_REQUEST_TYPE_FTO[]; /* request sending file */
-	extern char USER_REQUEST_TYPE_FOK[]; /* agree with receiving file */
-	extern char USER_REQUEST_TYPE_FNO[]; /* refuse receiving file */
-	extern char USER_REQUEST_TYPE_FRIENDLIST[]; /* request display friend list */
-	extern char USER_REQUEST_TYPE_ADDFRIEND[];  /* request add friend */
-	extern char USER_REQUEST_TYPE_ADDOK[];		/* agree with adding friend */
-	extern char USER_REQUEST_TYPE_ADDNO[];		/* refuse adding friend */
+	extern char USER_REQUEST_TYPE_MSG[]; 
+	extern char USER_REQUEST_TYPE_FTO[]; 
+	extern char USER_REQUEST_TYPE_FOK[]; 
+	extern char USER_REQUEST_TYPE_FNO[]; 
+	extern char USER_REQUEST_TYPE_FRIENDLIST[]; 
+	extern char USER_REQUEST_TYPE_ADDFRIEND[];  
+	extern char USER_REQUEST_TYPE_ADDOK[];	
+	extern char USER_REQUEST_TYPE_ADDNO[];		
 
-/* server */
-	/* registed user file */
+
+
 	extern char REGISTED_FILE[];
 	extern char FRIENDSHIP_FILE[];
 
-/* epoll */
+
 	const int MAX_EVENTS = 1024;
 
-/* file transfer */
-	/* file transfer attribute */
+
+
 	const int MAX_DIR_LEN       = 256;
 	const int MAX_FILE_NUM      = 3000;
 	const int FILE_TYPE_SIZE       = 10;
 	const int FILE_DATA_BLOCK_SIZE = MSG_DATA_SIZE - FILE_TYPE_SIZE;
 	/* file type */
-	extern char FILE_TYPE_DIR[]; /* file that will be transfered is a dir */
-	extern char FILE_TYPE_FB[];  /* the begining of transfer for a file */
-	extern char FILE_TYPE_FI[];  /* transfering a file */
-	extern char FILE_TYPE_FE[];  /* the end of transfer for a file */
+	extern char FILE_TYPE_DIR[]; 
+	extern char FILE_TYPE_FB[];  
+	extern char FILE_TYPE_FI[];  
+	extern char FILE_TYPE_FE[];  
 	/* receiving dir */
 	extern char RECEIVE_DIR[]; 
 
-/* ring queue */
+
 	const int RING_QUEUE_SIZE = 10240;
 	
 };
